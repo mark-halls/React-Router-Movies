13ea36b52a8cd1579fa47462677d1b3663d689c5
Mark Halls <mark.halls@gmail.com>
2019-10-15 14:15:28 -0700

    Move movie card component to separate file

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import MovieCard from "./MovieCard";
+
 const Movie = props => {
   const [movie, setMovie] = useState();
 
@@ -30,28 +32,30 @@ const Movie = props => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
+  return <MovieCard {...props} movie={movie} />;
 
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
-  );
+  // const { title, director, metascore, stars } = movie;
+  // return (
+  //   <div className="save-wrapper">
+  //     <div className="movie-card">
+  //       <h2>{title}</h2>
+  //       <div className="movie-director">
+  //         Director: <em>{director}</em>
+  //       </div>
+  //       <div className="movie-metascore">
+  //         Metascore: <strong>{metascore}</strong>
+  //       </div>
+  //       <h3>Actors</h3>
+
+  //       {stars.map(star => (
+  //         <div key={star} className="movie-star">
+  //           {star}
+  //         </div>
+  //       ))}
+  //     </div>
+  //     <div className="save-button">Save</div>
+  //   </div>
+  // );
 };
 
 export default Movie;
